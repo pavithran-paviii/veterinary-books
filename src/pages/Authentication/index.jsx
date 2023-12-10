441436ae0ef47429607f0b405cdeaa130c6fc84a
pavithran_paviii <pavithranr65@gmail.com>
2023-12-10 23:44:33 +0530

    Created authentication page

---
+++ src/pages/Authentication/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "./authentication.module.scss";
+
+const Authentication = () => {
+  return <div>Authentication</div>;
+};
+
+export default Authentication;
