b5d0e2dc5f51f78eb696ae41e667027c0ee02b42
pavithran_paviii <pavithranr65@gmail.com>
2024-05-31 02:26:55 +0530

    CC

--- src/assets/data/constant.js
+++ src/assets/data/constant.js
@@ -1,2 +1,2 @@
-// export const BACKENDURL = "https://backend.zafapetcare.com";
-export const BACKENDURL = "http://localhost:5000";
+export const BACKENDURL = "https://backend.zafapetcare.com";
+// export const BACKENDURL = "http://localhost:5000";
