1232edc9b40da2ca019e220b0f677f984f60206f
pavithran_paviii <pavithranr65@gmail.com>
2024-06-27 00:52:47 +0530

    Implemented till dark mode

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import Router from "./Router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import GlobalContextProvider from "./context/globalContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <GlobalContextProvider>
-      <Router />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        pauseOnHover
-      />
-    </GlobalContextProvider>
+    <ThemeProvider>
+      <GlobalContextProvider>
+        <Router />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          pauseOnHover
+        />
+      </GlobalContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/layout/Dashboard/index.jsx
+++ src/layout/Dashboard/index.jsx
@@ -8,17 +8,27 @@ import { IoIosSettings } from "react-icons/io";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../context/globalContext";
 import { IoLogOut } from "react-icons/io5";
+import useWindowSize from "../../hooks/useWindowSize";
+import { MaterialUISwitch } from "../../components/MUI";
+import { useTheme } from "../../context/ThemeContext";
 
 const DashboardLayout = ({ child }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const { email } = useContext(GlobalContext);
+  const { height } = useWindowSize();
+  const { theme, toggleTheme } = useTheme();
 
   //functions
+
   function logoutFunc() {
     localStorage.clear();
     navigate("/");
   }
+
+  const handleModeChange = (event) => {
+    toggleTheme();
+  };
 
   if (!email) {
     return <Navigate to="/signin" />;
@@ -26,28 +36,32 @@ const DashboardLayout = ({ child }) => {
   return (
     <section className={classNames.dashboardLayout}>
       <div className={classNames.leftSidebar}>
-        <div className={classNames.logo}>
-          <img src={logoWhite} alt="logoWhite" />
-        </div>
         <div className={classNames.dashItems}>
-          {dashboardItems?.map((eachItem, index) => {
-            return (
-              <div
-                className={`${classNames.sidebarItem} ${
-                  location?.pathname?.includes(eachItem?.name?.toLowerCase())
-                    ? classNames.selectedItem
-                    : ""
-                }`}
-                key={eachItem?.name + index}
-                onClick={() => {
-                  navigate(`/${eachItem?.name?.toLowerCase()}`);
-                }}
-              >
-                {eachItem.icon}
-                <span>{eachItem?.name}</span>
-              </div>
-            );
-          })}
+          <div className={classNames.logo}>
+            <img src={logoWhite} alt="logoWhite" />
+          </div>
+          <div className={classNames.dashItems}>
+            {dashboardItems?.map((eachItem, index) => {
+              return (
+                <div
+                  className={`${classNames.sidebarItem} ${
+                    location?.pathname?.includes(eachItem?.name?.toLowerCase())
+                      ? classNames.selectedItem
+                      : ""
+                  }`}
+                  key={eachItem?.name + index}
+                  onClick={() => {
+                    navigate(`/${eachItem?.name?.toLowerCase()}`);
+                  }}
+                >
+                  <span>{eachItem.icon}</span>
+                  <span>{eachItem?.name}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={classNames.settingsMenu}>
           <div
             className={`${classNames.sidebarItem} ${classNames.otherItems} ${
               location?.pathname?.includes("settings")
@@ -58,16 +72,36 @@ const DashboardLayout = ({ child }) => {
               navigate(`/settings`);
             }}
           >
-            <IoIosSettings />
+            <span>
+              <IoIosSettings />
+            </span>
             Settings
           </div>
           <div
             className={`${classNames.sidebarItem} ${classNames.logoutBtn}`}
             onClick={logoutFunc}
           >
-            <IoLogOut />
+            <span>
+              <IoLogOut />
+            </span>
             Logout
           </div>
+          {height > 700 && height > 500 && (
+            <MaterialUISwitch
+              sx={{ width: 60, margin: "0 auto" }}
+              checked={theme === "dark" ? true : false}
+              onChange={handleModeChange}
+            />
+          )}
+          {height > 750 && (
+            <div className={classNames.needHelp}>
+              <div className={classNames.title}>Need help?</div>
+              <div className={classNames.desc}>
+                Wanted new features or having trouble?
+              </div>
+              <div className={classNames.submitBtn}>Write us here</div>
+            </div>
+          )}
         </div>
       </div>
       <div className={classNames.rightLayout}>{child}</div>
