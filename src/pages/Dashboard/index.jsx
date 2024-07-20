322122772bdbbd5c0eedfc1a572f3da08b42cf21
pavithran_paviii <pavithranr65@gmail.com>
2024-07-20 05:50:42 +0530

    Implemented till dashboard UI & inventory functionality

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,22 +1,31 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classNames from "./dashboard.module.scss";
 import axios from "axios";
 import { BACKENDURL } from "../../assets/data/constant";
 import { GlobalContext } from "../../context/globalContext";
+import { CustomSelectOne } from "../../components/Custom";
 
 const Dashboard = () => {
   const { token } = useContext(GlobalContext);
+  const [selectedDashboard, setSelectedDashboard] = useState({
+    dashItem: "Financial",
+  });
+  const [dashboardStats, setDashboardStats] = useState({});
 
   //functions
 
   async function getDashboard() {
     try {
-      let response = await axios.get(BACKENDURL + "/inventory/getbills", {
+      let response = await axios.get(BACKENDURL + "/inventory/stats", {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       });
+
+      if (response?.data?.status) {
+        setDashboardStats(response?.data?.data);
+      }
       console.log(response, "Get dashboard stats response");
     } catch (error) {
       console.log(error?.message, "Get dashboard stats error");
@@ -32,8 +41,26 @@ const Dashboard = () => {
   return (
     <div className={classNames.dashboard}>
       <div className={classNames.header}>
-        <div className={classNames.title}>Revenue</div>
-        <div className={classNames.value}></div>
+        <div>
+          <div className={classNames.title}>Revenue</div>
+          <div className={classNames.value}>
+            ₹{" "}
+            {dashboardStats?.totalSales
+              ? Number(dashboardStats?.totalSales)?.toFixed(2)
+              : "0.00"}
+          </div>
+        </div>
+        <CustomSelectOne
+          stateValue={selectedDashboard}
+          setState={setSelectedDashboard}
+          name="dashItem"
+          allOptions={[
+            { name: "Financial" },
+            { name: "Inventory" },
+            { name: "Pets" },
+            { name: "Clients" },
+          ]}
+        />
       </div>
     </div>
   );
--- src/pages/Inventory/index.jsx
+++ src/pages/Inventory/index.jsx
@@ -1,13 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import classNames from "./inventory.module.scss";
 import { useNavigate } from "react-router-dom";
-import CustomInput, { CustomButton, Toastify } from "../../components/Custom";
+import CustomInput, {
+  CustomButton,
+  CustomSelectOne,
+  Toastify,
+} from "../../components/Custom";
 import axios from "axios";
 import { BACKENDURL } from "../../assets/data/constant";
 import { GlobalContext } from "../../context/globalContext";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline } from "react-icons/md";
 import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
 
 const Inventory = () => {
   const navigate = useNavigate();
@@ -17,7 +22,8 @@ const Inventory = () => {
   const [selectedMedicine, setSelectedMedicine] = useState("");
   const [increaseInventory, setIncreaseInventory] = useState(false);
   const [inventoryForm, setInventoryForm] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [finalCart, setFinalCart] = useState({});
 
   //functions
 
@@ -77,6 +83,101 @@ const Inventory = () => {
       });
   }
 
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find(
+        (item) => item.item === product._id
+      );
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.item === product._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [
+          ...prevCart,
+          {
+            item: product._id,
+            quantity: 1,
+            price: product.retailPrice,
+            name: product.medicineName,
+            mrp: product.mrp,
+          },
+        ];
+      }
+    });
+  };
+
+  const increaseQuantity = (productId) => {
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.item === productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (productId) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) =>
+          item.item === productId && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.item !== productId));
+  };
+
+  const calculateTotalCost = (items) => {
+    return items.reduce((total, item) => {
+      return total + item.quantity * parseFloat(item.price);
+    }, 0);
+  };
+
+  const removeFields = (items) => {
+    return items.map(({ name, mrp, ...rest }) => rest);
+  };
+
+  async function submitBill() {
+    const updatedCartItems = removeFields(cart);
+    finalCart.totalAmount = calculateTotalCost(cart);
+    finalCart.items = updatedCartItems;
+
+    try {
+      let response = await axios.post(
+        BACKENDURL + "/inventory/billing",
+        finalCart,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response, "response cart added!");
+      if (response?.data?.status) {
+        Toastify("Bill Successful!", "success");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      } else {
+        Toastify(response?.data?.message, "error");
+      }
+    } catch (error) {
+      Toastify("Server error while submitting bill", "error");
+      console.log(error?.message, "Server error while submitting bill");
+    }
+  }
+
+  //renderings
+
   useEffect(() => {
     getAllInventory();
   }, []);
@@ -96,91 +197,72 @@ const Inventory = () => {
         </div>
         <div className={classNames.allProducts}>
           {Array.isArray(allInventory) &&
-            allInventory?.length > 0 &&
+            allInventory.length > 0 &&
             allInventory
-              ?.filter((eachinventory) => {
+              .filter((eachinventory) => {
                 let searchText = searchQuery?.toLowerCase();
                 return eachinventory?.medicineName
                   ?.toLowerCase()
                   ?.includes(searchText);
               })
-              ?.map((eachProduct) => {
+              .map((eachProduct) => {
+                const cartItem = cart.find(
+                  (item) => item.item === eachProduct._id
+                );
+
                 return (
-                  <div
-                    className={classNames.eachProduct}
-                    key={eachProduct?._id}
-                  >
+                  <div className={classNames.eachProduct} key={eachProduct._id}>
                     <div className={classNames.detailsContainer}>
                       <div className={classNames.imageContainer}>
-                        {eachProduct?.medicineName?.charAt(0)}
+                        {eachProduct.medicineName.charAt(0)}
                       </div>
                       <div className={classNames.details}>
                         <div className={classNames.name}>
-                          {eachProduct?.medicineName}
+                          <span>{eachProduct.medicineName}</span>{" "}
+                          <span>( {eachProduct.quantity} items available)</span>
                         </div>
                         <div className={classNames.price}>
-                          <span>₹ {eachProduct?.retailPrice}</span>
-                          <span>₹ {eachProduct?.mrp}</span>
+                          <span>₹ {eachProduct.retailPrice}</span>
+                          <span>₹ {eachProduct.mrp}</span>
                         </div>
                       </div>
                     </div>
                     <div className={classNames.controlBtns}>
-                      <FaCircleMinus />
-                      <input type="text" placeholder="0" />
-                      <FaCirclePlus />
+                      <FaCircleMinus
+                        onClick={() => decreaseQuantity(eachProduct._id)}
+                      />
+                      <input
+                        type="text"
+                        value={cartItem?.quantity || 0}
+                        readOnly
+                      />
+                      <FaCirclePlus
+                        className={
+                          eachProduct.quantity <= cartItem?.quantity &&
+                          classNames.notAllowedBtn
+                        }
+                        onClick={() => addToCart(eachProduct)}
+                      />
                     </div>
                   </div>
                 );
               })}
         </div>
-        {/* <div
-          className={classNames.tableContainer}
-          style={{ display: allInventory?.length > 0 ? "" : "none" }}
-        >
-          <table>
-            <thead>
-              <tr>
-                <th>Medicine Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Description</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {allInventory?.length > 0 &&
-                allInventory
-                  ?.filter((eachinventory) => {
-                    let searchText = searchQuery?.toLowerCase();
-                    return eachinventory?.medicineName?.includes(searchText);
-                  })
-                  .map((eachItem, index) => {
-                    return (
-                      <tr key={eachItem?.name + index}>
-                        <td>{eachItem?.medicineName}</td>
-                        <td>{eachItem?.quantity}</td>
-                        <td>
-                          {eachItem?.price ? `₹ ${eachItem?.price}` : "-"}
-                        </td>
-                        <td>{eachItem?.description}</td>
-                        <td>
-                          <CiEdit
-                            onClick={() => {
-                              setSelectedMedicine(eachItem?.medicineName);
-                              setIncreaseInventory([]);
-                            }}
-                          />
-                        </td>
-                        <td>
-                          <MdDeleteOutline />
-                        </td>
-                      </tr>
-                    );
-                  })}
-            </tbody>
-          </table>
-        </div> */}
+        <div className={classNames.totalBill}>
+          <div className={classNames.header}>
+            <div className={classNames.title}>
+              <div>Order Summary</div>
+              <div>(Total Amount)</div>
+            </div>
+          </div>
+          <div className={classNames.price}>
+            ₹{" "}
+            {calculateTotalCost(cart)
+              ? calculateTotalCost(cart)?.toFixed(2)
+              : "0.00"}
+          </div>
+        </div>
+
         {/* {increaseInventory && (
           <div className={classNames.increaseInventory}>
             <div
@@ -240,7 +322,69 @@ const Inventory = () => {
         )} */}
       </div>
       <div className={classNames.cartContainer}>
-        <div className={classNames.title}>Cart</div>
+        <div className={classNames.title}>
+          Cart
+          <div className={classNames.paymentType}>
+            <CustomSelectOne
+              stateValue={finalCart}
+              setState={setFinalCart}
+              name="billingType"
+              allOptions={[{ name: "Online" }, { name: "Cash" }]}
+            />
+          </div>
+        </div>
+
+        <div className={classNames.cartItems}>
+          {Array.isArray(cart) &&
+            cart?.length > 0 &&
+            cart?.map((eachCartItem) => {
+              return (
+                <div className={classNames.eachProduct} key={eachCartItem.item}>
+                  <div className={classNames.detailsContainer}>
+                    <div className={classNames.imageContainer}>
+                      {eachCartItem.name.charAt(0)}
+                    </div>
+                    <div className={classNames.details}>
+                      <div className={classNames.name}>{eachCartItem.name}</div>
+                      <div className={classNames.price}>
+                        <span>₹ {eachCartItem.price}</span>
+                        <span>₹ {eachCartItem.mrp}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classNames.cartBtns}>
+                    <div className={classNames.eachTotal}>
+                      <div>{eachCartItem?.quantity} Quantity</div>
+                      <div>
+                        ₹{" "}
+                        {(
+                          eachCartItem?.quantity * eachCartItem?.price
+                        )?.toFixed(2)}
+                      </div>
+                    </div>
+                    <div
+                      className={classNames.deleteBtn}
+                      onClick={() => removeFromCart(eachCartItem?.item)}
+                    >
+                      <MdDelete />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+        <div
+          className={`${classNames.paymentBtn} ${
+            !(
+              Array.isArray(cart) &&
+              cart?.length > 0 &&
+              finalCart?.billingType
+            ) && classNames.notAllowedBtn
+          }`}
+          onClick={submitBill}
+        >
+          Confirm Payment
+        </div>
       </div>
     </div>
   );
