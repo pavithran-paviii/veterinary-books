dc3cef814fa0e2a74af149a02a4234f2bd800751
pavithran_paviii <pavithranr65@gmail.com>
2024-07-29 01:18:58 +0530

    Implemented pet records

--- src/pages/PetRecords/index.jsx
+++ src/pages/PetRecords/index.jsx
@@ -1,11 +1,50 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classNames from "./petrecords.module.scss";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../context/globalContext";
+import { BACKENDURL } from "../../assets/data/constant";
+import { Toastify } from "../../components/Custom";
+import axios from "axios";
+import DynamicTable from "../../components/DynamicTable";
 
 const PetRecords = () => {
   const navigate = useNavigate();
-  const { searchQuery, setSearchQuery } = useContext(GlobalContext);
+  const { searchQuery, setSearchQuery, token } = useContext(GlobalContext);
+  const [allPetRecords, setAllPetRecords] = useState([]);
+
+  //functions
+
+  function getAllMedicalRecords() {
+    axios
+      .get(BACKENDURL + `/medicalrecord`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        if (response?.data?.status) {
+          setAllPetRecords(response?.data?.data);
+        }
+        console.log(response, "all pet records response");
+      })
+      .catch((error) => {
+        console.log(error, "all pet records error");
+        Toastify(
+          error?.response?.data?.message
+            ? error?.response?.data?.message
+            : error?.message,
+          "error",
+          "error"
+        );
+      });
+  }
+
+  //renderings
+
+  useEffect(() => {
+    getAllMedicalRecords();
+  }, []);
 
   return (
     <div className={classNames.client}>
@@ -19,7 +58,32 @@ const PetRecords = () => {
           Add Records
         </button>
       </div>
-      <div></div>
+      <DynamicTable
+        tableHead={[
+          "Name",
+          "Age",
+          "Gender",
+          "Weight",
+          "Clinical Signs",
+          "Treatment",
+        ]}
+        mapArray={allPetRecords}
+        tableBody={[
+          "name",
+          "age",
+          "sex",
+          "weight",
+          "clinicalSigns",
+          "treatment",
+        ]}
+        searchQuery={searchQuery}
+        // outlineFunc={deletePet}
+        // mainFunc={(obj) => {
+        //   setLocalStep(obj?._id);
+        //   setSelectedPet(obj);
+        // }}
+        filterItem={"name"}
+      />
     </div>
   );
 };
